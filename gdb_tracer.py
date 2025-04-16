b395d65aa92a05c67f0b29e1e7cefe18d0287d63
Anjali Sreenivasan <anjsreenivasan@gmail.com>
2025-04-16 17:11:45 -0400

    slayyyyy

---
+++ gdb_tracer.py
@@ -0,0 +1,25 @@
+import gdb
+import json
+
+class RunAll(gdb.Command):
+    def __init__(self):
+        # register command name in gdb
+        super(RunAll, self).__init__("runall", gdb.COMMAND_USER)
+        # initialize empty list to hold trace data at each line
+        self.trace_data = []
+
+    # run gdb on the program and save data
+    def invoke(self, args, from_tty):
+        # start running gdb at the beggining of main
+        gdb.execute("start")
+        # infinite loop to step through every line of code
+        while True:
+            try:
+                # get current stack frame
+                stack = gdb.selected_frame()
+                # get current scope
+                scope = stack.block()
+                # create dictionary {variable: value}
+                vars = {}
+                #
+
