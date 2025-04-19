ecbd3a0e132ddb0caf3bfe4eb250151e242588c5
Anjali Sreenivasan <anjsreenivasan@gmail.com>
2025-04-19 15:02:38 -0400

    gdb.next

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 GDB python script will run GDB on the program and store all variables with line numbers
 Load the JSON into an ai system
 User asks ai questions about errors in code
+
+possible errors
+- state of variables out of the scope are still saved in the vars dictionary ex(for loop in c)
+    - maybe if var no longer in scope take it out of dictionary??
--- gdb_tracer.py
+++ gdb_tracer.py
@@ -1,11 +1,12 @@
 import gdb
 import json
 
+# custom gdb command
 class RunAll(gdb.Command):
     def __init__(self):
         # register command name in gdb
         super(RunAll, self).__init__("runall", gdb.COMMAND_USER)
-        # initialize empty list to hold trace data at each line
+        # initialize empty list to hold data at each line
         self.trace_data = []
 
     # run gdb on the program and save data
@@ -14,24 +15,35 @@ class RunAll(gdb.Command):
         gdb.execute("start")
         # infinite loop to step through every line of code
         while True:
+            # for each line
             try:
                 # get current stack frame
                 stack = gdb.selected_frame()
                 # get current scope
                 scope = stack.block()
-                # create dictionary {variable: value}
+                # create dictionary {symbol: value}
                 vars = {}
                 # loop through code and find variables
+                # symbol is an object with a set of functions you can use, automatically an object when u iterate scope
                 for symbol in scope:
                     if symbol.is_variable:
                         try:
-                            # rea value of variable in current frame
+                            # read value of variable in current frame, uses name of symbol to get value
                             value = stack.read_var(symbol.name)
+                            # save symbol & string version of value to dictionary (string for json later)
+                            vars[symbol.name] = str(value)
                         except:
-                            #idk whatever
-                        # curr line number using program counter
+                            vars[symbol.name] = "undefined"
+                        # get the curr line number where the symbol is using program counter
                         line = gdb.find_pc_line(stack.pc).line
-                        
+                        # add to trace data dictionary, {line num (int): updated vars dictionary}
+                        self.trace_data[line] = vars
+                # next line
+                gdb.execute("next")
+
+            except:
+                # end of program
+
 
 
 
